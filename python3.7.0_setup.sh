3fb7ebbb109a61c545772f9867fed6d320bd56ff
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2021-04-28 14:38:56 +0900

    Update python3.7.0_setup.sh

--- python3.7.0_setup.sh
+++ python3.7.0_setup.sh
@@ -22,22 +22,11 @@ case $yn in
 esac
 
 # ubuntu's basic program list.
-sudo apt-get install -y build-essential
-sudo apt-get install -y checkinstall
-sudo apt-get install -y libreadline-gplv2-dev
-sudo apt-get install -y libncursesw5-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libsqlite3-dev
-sudo apt-get install -y tk-dev
-sudo apt-get install -y libgdbm-dev
-sudo apt-get install -y libc6-dev
-sudo apt-get install -y libbz2-dev
-sudo apt-get install -y zlib1g-dev
-sudo apt-get install -y openssl
-sudo apt-get install -y libffi-dev
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-setuptools
-sudo apt-get install -y wget
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz  # download python3.7.0
 tar xvf Python-3.7.0.tar.xz
 cd Python-3.7.0
@@ -58,33 +47,6 @@ echo "pip install"
 sudo apt-get install python3-pip -y  # python3's pip install
 
 echo "  "
-
-echo "Docker install"
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-    
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-echo "Nvidia Docker install"
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-sudo systemctl restart docker
 
 read -p "System will reboot. restart now? [y/n] " yn
 case $yn in
