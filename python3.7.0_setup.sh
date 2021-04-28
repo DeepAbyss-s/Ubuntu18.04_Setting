4860e11cb737f64510b977f5d7d825cd7917eccc
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2021-04-28 18:21:17 +0900

    Update python3.7.0_setup.sh

--- python3.7.0_setup.sh
+++ python3.7.0_setup.sh
@@ -1,26 +1,3 @@
-dver=$(. /etc/os-release;echo $ID$VERSION_ID)  #Ubuntu's 18.04 version check.
-arch=$(dpkg -s libc6 | grep Architecture)  #Ubuntu's 18.04 64bit version check.
-
-echo " check ubuntu version !"
-
-if [ ${dver//.} = ubuntu1804 ];then
-  echo "Version : ${dver}"
-else
-  echo "Sorry! For Ubuntu 18.04 LTS Version !!"  // for only ubuntu 18.04 version!!
-  exit
-fi
-
-echo "    "
-read -p "If you continue this process, python3.7.0 version will install. Do it? [y/n]" yn
-case $yn in
-  [Yy]* ) echo "  "
-    echo "Start!"
-    echo "    ";;
-  [Nn]* ) echo "  "
-    echo " thank you for use our Service!"
-    exit;;
-esac
-
 # ubuntu's basic program list.
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -46,12 +23,5 @@ echo " "
 echo "pip install"
 sudo apt-get install python3-pip -y  # python3's pip install
 
-echo "  "
-
-read -p "System will reboot. restart now? [y/n] " yn
-case $yn in
-  [Yy]* ) echo " System will restart ! "
-    sudo reboot;;
-  [Nn]* ) echo " Proceed your work."
-    exit;;
-esac
+# 도커설치진행
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
